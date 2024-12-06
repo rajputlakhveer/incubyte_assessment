d60c484dae0479e3348685261c12970cc9255621
Lakhveer Singh Rajput <lakhveersinghrajput@Lakhveers-MacBook-Air.local>
2024-12-06 11:10:40 +0530

    Negative Error

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,11 @@
 class StringCalculator
 	def self.add(numbers)
 		return 0 if numbers.empty?
-		custom_delimiter = numbers.start_with?("//") ? numbers[2] : ''
-		numbers.split(/,|\n|#{custom_delimiter}/).map{|n| n.to_i }.inject(:+)
+		custom_delimiter = numbers.start_with?("//") ? numbers[2] : ' '
+		numbers = numbers.split(/,|\n|#{custom_delimiter}/).map{|n| n.to_i }
+		negative_numbers = numbers.select { |n| n < 0 }
+
+		return "negative numbers not allowed #{negative_numbers}" unless negative_numbers.empty?
+		numbers.inject(:+)
 	end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,15 +9,19 @@ describe 'StringCalculator' do
     expect(StringCalculator.add("5")).to eq(5)
   end
 
-  it 'returns number when one number string provided' do
+  it 'returns number when multiple number string provided' do
     expect(StringCalculator.add("1,2,3")).to eq(6)
   end
 
-  it 'returns number when one number string provided' do
+  it 'returns number when new line delimiter in numbers string provided' do
     expect(StringCalculator.add("1\n2,3")).to eq(6)
   end
 
-  it 'returns number when one number string provided' do
+  it 'returns number when custom delimiter fixed in a numbers string provided' do
     expect(StringCalculator.add("//;\n1;2")).to eq(3)
+  end
+
+  it 'returns error message when negative number string provided' do
+    expect(StringCalculator.add("1,-2,3")).to eq("negative numbers not allowed [-2]")
   end
 end
