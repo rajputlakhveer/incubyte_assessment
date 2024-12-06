ad6385b8f7ff020a2796189958da6c7ab98d2dea
Lakhveer Singh Rajput <lakhveersinghrajput@Lakhveers-MacBook-Air.local>
2024-12-06 09:49:15 +0530

    Test Case for Single Number

---
+++ lib/string_calculator.rb
@@ -0,0 +1,5 @@
+class StringCalculator
+	def self.add(numbers)
+		numbers.to_i
+	end
+end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,11 @@
+require_relative './../lib/string_calculator'
+
 describe 'StringCalculator' do
   it 'returns 0 for an empty string' do
     expect(StringCalculator.add("")).to eq(0)
+  end
+
+    it 'returns number when one number string provided' do
+    expect(StringCalculator.add("5")).to eq(5)
   end
 end
